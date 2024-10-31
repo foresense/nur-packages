92a3e9eff686e58e31bcc13f7fd970f401e9a8be
Robert Beenen <robert@foresen.se>
2024-10-31 21:49:12 +0100

    allowUnfree = true

--- flake.nix
+++ flake.nix
@@ -16,7 +16,13 @@
     in
     {
       legacyPackages = forAllSystems (
-        system: import ./default.nix { pkgs = import nixpkgs { inherit system; }; }
+        system:
+        import ./default.nix {
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        }
       );
       packages = forAllSystems (
         system: nixpkgs.lib.filterAttrs (_: v: nixpkgs.lib.isDerivation v) self.legacyPackages.${system}
